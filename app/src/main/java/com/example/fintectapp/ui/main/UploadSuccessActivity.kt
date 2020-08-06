683e37b7a1f2126cad0e3821b3f4428076665531
hyojini22 <gywls990129@gmail.com>
2020-08-06 20:37:36 +0900

    Make an action for a button in UploadSuccessActivity

--- app/src/main/java/com/example/fintectapp/ui/main/UploadSuccessActivity.kt
+++ app/src/main/java/com/example/fintectapp/ui/main/UploadSuccessActivity.kt
@@ -1,12 +1,27 @@
 package com.example.fintectapp.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.example.fintectapp.MainActivity
 import com.example.fintectapp.R
+import com.example.fintectapp.ui.main.contents.HistoryContent
+import kotlinx.android.synthetic.main.activity_upload_success.*
 
 class UploadSuccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload_success)
+
+        button_to_home.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        button_to_main.setOnClickListener {
+            TODO("해당 화면 보여주기")
+        }
+
     }
 }
