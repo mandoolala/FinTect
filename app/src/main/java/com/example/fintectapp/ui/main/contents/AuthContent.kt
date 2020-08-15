adc4edc536ffd2b4620442e0ae1f0bcbf649ce4f
Euicheon <no1eclee@gmail.com>
2020-08-15 23:27:43 +0900

    Not Yet Finished..

--- app/src/main/java/com/example/fintectapp/ui/main/contents/AuthContent.kt
+++ app/src/main/java/com/example/fintectapp/ui/main/contents/AuthContent.kt
@@ -1,10 +1,13 @@
 package com.example.fintectapp.ui.main.contents
 
 import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
-import java.util.ArrayList
-import java.util.HashMap
-
+import com.google.firebase.firestore.QuerySnapshot
+import java.util.*
+import kotlinx.coroutines.*
 /**
  * Helper class for providing sample content for user interfaces created by
  * Android template wizards.
@@ -24,18 +27,75 @@ object AuthContent {
     val ITEM_MAP: MutableMap<String, DummyItem> = HashMap()
 
     private var COUNT = 0
-
+    var FINISHED = false
     init {
+//        Log.e("DATA","LODAED 1 ")
+//
+//        // Add items(collection : "company") from firestore where flag is true
+//        val query = db.collection("company").whereEqualTo("flag", false)
+//        Log.e("DATA","LODAED 2 ")
+//
+//        val task: Task<QuerySnapshot> = query.get()
+//        Log.e("DATA","LODAED 3 ")
+////        val snap: QuerySnapshot? = Tasks.await(task)
+//        Log.e("DATA","LODAED 4 ")
+//
+//        val registration = query.addSnapshotListener { documents, e ->
+//            if (e != null) {
+//                Log.w("DB", "Listen failed.", e)
+//                return@addSnapshotListener
+//            }
+//            Log.e("DATA","LODAED 5 ")
+//
+//            for (document in documents!!) {
+//                Log.e("DATA", "${document?.data}")
+//                val name: String = document?.data?.get("name") as String
+//                val position: Int = (document?.data?.get("position") as Long)?.toInt()
+//                val date: String = document?.data?.get("date") as String
+//                val flag: Boolean = document?.data?.get("flag") as Boolean
+////                addItem(createDummyItem(name, position, date, flag))
+////                COUNT += 1
+//            }
+//            FINISHED = true
+//        }
+//
+//        if (COUNT > 0) {
+//            registration.remove()
+//        } else {
+//            registration
+//        }
+
+    }
+
+
+    fun refreshDB() {
+        Log.e("AUTH","LODAED 1 ")
+
         // Add items(collection : "company") from firestore where flag is true
         val query = db.collection("company").whereEqualTo("flag", false)
-        val registration = query.addSnapshotListener { documents, e ->
+        Log.e("AUTH","LODAED 2 ")
+
+        runBlocking<Unit> {
+            launch {
+
+                delay(1000L)
+                Log.e("TAG", "World")
+
+            }
+            Log.e("TAG", "Hello")
+            val task: Task<QuerySnapshot> = query.get()
+            Log.e("AUTH","LODAED 3 ")
+//            val snap: QuerySnapshot? = Tasks.await(task)
+
+            val registration = query.addSnapshotListener { documents, e ->
                 if (e != null) {
                     Log.w("DB", "Listen failed.", e)
                     return@addSnapshotListener
                 }
+                Log.e("AUTH","LODAED 4 ")
 
                 for (document in documents!!) {
-                    Log.d("DATA", "${document?.data}")
+                    Log.e("AUTH", "${document?.data}")
                     val name: String = document?.data?.get("name") as String
                     val position: Int = (document?.data?.get("position") as Long)?.toInt()
                     val date: String = document?.data?.get("date") as String
@@ -43,15 +103,19 @@ object AuthContent {
                     addItem(createDummyItem(name, position, date, flag))
                     COUNT += 1
                 }
+                FINISHED = true
             }
 
-        if (COUNT > 0) {
-            registration.remove()
-        } else {
-            registration
+            if (COUNT > 0) {
+                registration.remove()
+            } else {
+                registration
+            }
+            delay(1000L)
         }
-    }
 
+
+    }
     private fun addItem(item: DummyItem) {
         ITEMS.add(item)
         ITEM_MAP.put(item.id, item)
